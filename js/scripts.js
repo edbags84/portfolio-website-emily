cedb5d6d0ed18b338c4843f67fb7c1e5f64a8980
William Pei Yuan <williampeiyuan@gmail.com>
2024-12-23 15:11:16 -0500

    template portfolio for emily

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function menuToggle() {
+  var x = document.getElementById("myNavtoggle");
+  var isMobileBreakpoint = window.innerWidth <= 641;
+
+  if (x.className === "navtoggle" && isMobileBreakpoint) {
+    x.className += " responsive";
+  } else {
+    x.className = "navtoggle";
+  }
+}
